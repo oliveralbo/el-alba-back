4d254970ce7fb420d6d80b6249839784162f1910
oliveralbo <oliveralbo@hotmail.com>
2021-12-26 20:16:10 -0300

    feat: update postman collection && proceso quantity

--- server/routes/process.js
+++ server/routes/process.js
@@ -6,55 +6,46 @@ const { authToken } = require("../middlewares/auth");
 
 //PUT
 app.put("/proceso", authToken, function (req, res) {
+  let prod = Object.keys(req.body.pro);
+  let valueProd = Object.values(req.body.pro);
 
+  let idProduc = prod[0];
 
-    let prod = Object.keys(req.body.pro)
-    let valueProd = Object.values(req.body.pro)
+  Producto.findByIdAndUpdate(
+    idProduc,
+    { $inc: { quantity: valueProd[0] } },
+    { new: true, runValidators: true },
+    (err, registryDB) => {
+      if (err) {
+        return res.status(400).json({ ok: false, err });
+      }
+    }
+  );
 
-    let idProduc = prod[0];
+  let rawMaterial = Object.keys(req.body.mp);
+  let valueRawMat = Object.values(req.body.mp);
+  let cant = rawMaterial.length;
 
-    Producto.findByIdAndUpdate(
-      idProduc,
-      { $inc: {"quantity" : valueProd[0]}},
+  for (let i = 0; i < cant; i++) {
+    let idrawMaterial = rawMaterial[i];
+
+    MateriaPrima.findByIdAndUpdate(
+      idrawMaterial,
+      { $inc: { quantity: -valueRawMat[i] } },
       { new: true, runValidators: true },
       (err, registryDB) => {
         if (err) {
           return res.status(400).json({ ok: false, err });
         }
-
-      },
+      }
     );
+  }
 
-
-    let rawMaterial = Object.keys(req.body.mp)
-    let valueRawMat = Object.values(req.body.mp)
-    let cant = rawMaterial.length
-
-    for(let i =0; i<cant;i++){
-
-        let idrawMaterial = rawMaterial[i];
-
-        MateriaPrima.findByIdAndUpdate(
-            idrawMaterial,
-            { $inc: {"quantity" : -valueRawMat[0]}},
-          { new: true, runValidators: true },
-          (err, registryDB) => {
-            if (err) {
-              return res.status(400).json({ ok: false, err });
-            }
-    
-          },
-        );
-    }
-
-res.json({
+  res.json({
     ok: true,
     registry: "Proceso realizado con exito",
     reqUser: req.authUser.email,
-})
-   
- 
-  
+  });
 });
 
 module.exports = app;
